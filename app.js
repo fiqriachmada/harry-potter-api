18484c5d77d2a957f8b5836be5c094d615e40c8b
ACHMADA FIQRI A. RASYAD <38800157+fiqriachmada@users.noreply.github.com>
2022-06-17 07:36:09 +0000

    adding cors

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 import express from "express";
 import dotenv from "dotenv";
 import mysql from "mysql2/promise";
+import cors from "cors";
+import bodyParser from "body-parser";
+import fs from "fs";
+import path from "path";
+// var CHARACTERS_JSON = path.join(__dirname, 'data/characters.json');
+
+
 dotenv.config();
 
 const connection = await mysql.createConnection(process.env.DATABASE_URL);
-console.log("The Database is connected to\n" + connection);
+console.log("The Database is connected to " + process.env.DATABASE_URL + "\n");
 
 const app = express();
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.json({ msg: "Hello World" });
@@ -16,16 +27,20 @@ let status = 200;
 let returnValue = {};
 
 app.get("/characters", async (req, res) => {
+
   try {
     const query = "SELECT * FROM hp_character";
     const [rows] = await connection.query(query);
     returnValue.data = rows;
+    console.log(rows)
+    res.setHeader('Access-Control-Allow-Origin', '*');
   } catch (error) {
     console.log(error);
     status = 500;
     returnValue.msg = "Something went wrong";
   } finally {
     res.status(status).json(returnValue);
+    console.log(status);
   }
 });
 
@@ -57,6 +72,7 @@ app.get("/wands/:id", async (req, res) => {
   res.json(rows[0]);
 });
 
-app.listen(3000, () => {
-  console.log("App is Listening...and the server is up\n");
+
+const listener = app.listen(process.env.PORT, function() {
+  console.log('Your app is listening on port ' + listener.address().port);
 });
