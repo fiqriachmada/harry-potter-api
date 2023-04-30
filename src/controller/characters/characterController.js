e6a626c6c7de55416e79d1ca22a50fcd55c6c2f4
fiqriachmada <achmada@1998.com>
2023-04-30 17:59:54 +0700

    new update

--- src/controller/characters/characterController.js
+++ src/controller/characters/characterController.js
@@ -10,7 +10,7 @@ characterController.get('/', async (req, res) => {
   const itemsPerPage = 10
   const page = parseInt(req.query.page) || 1 // use query parameter or default to 1
   const offset = (page - 1) * itemsPerPage
-  const query = `SELECT * FROM hp_character LIMIT ${itemsPerPage} OFFSET ${offset} ORDER BY id DESC`
+  const query = `SELECT * FROM hp_character ORDER BY id DESC LIMIT ${itemsPerPage} OFFSET ${offset}`
 
   const [rows] = await (await connection).query(query)
 
