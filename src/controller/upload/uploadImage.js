58c8651d16f47518e26ffd6ebe8e0f8e61f82f5e
fiqriachmada <achmada1998@gmail.com>
2023-05-09 21:02:24 +0700

    new

--- src/controller/upload/uploadImage.js
+++ src/controller/upload/uploadImage.js
@@ -1,16 +1,7 @@
 import multer from 'multer';
+import fs from 'fs';
 import { Router } from 'express';
 
-// const storage = multer.diskStorage({
-//   destination: (req, file, cb) => {
-//     cb(null, 'upload/'); // set the upload directory
-//   },
-//   filename: (req, file, cb) => {
-//     cb(null, file.originalname); // set the filename to the original name of the file
-//   },
-// });
-
-const fs = require('fs');
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     const uploadDir = './uploads'; // create a new directory called "uploads"
