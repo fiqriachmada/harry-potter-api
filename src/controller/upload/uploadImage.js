356fcf7190d84d3d8e4c2812c176863f23c600f8
fiqriachmada <achmada1998@gmail.com>
2023-05-09 20:52:09 +0700

    new

--- src/controller/upload/uploadImage.js
+++ src/controller/upload/uploadImage.js
@@ -1,11 +1,9 @@
-import express from 'express';
+import multer from 'multer';
 import { Router } from 'express';
 
-// configure multer middleware to handle file uploads
-const multer = require('multer');
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'uploads/'); // set the upload directory
+    cb(null, 'upload/'); // set the upload directory
   },
   filename: (req, file, cb) => {
     cb(null, file.originalname); // set the filename to the original name of the file
@@ -16,7 +14,7 @@ const upload = multer({ storage: storage });
 const uploadImage = Router();
 
 // define a route for handling file uploads
-uploadImage.post('/upload', upload.single('file'), (req, res) => {
+uploadImage.post('/upload', upload.single('filepond'), (req, res) => {
   console.log(req.file); // log the file data for debugging purposes
   res.send('File uploaded successfully!');
 });
