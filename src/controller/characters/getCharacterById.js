be98ef3bc60e00d94e2faedb928678f2a084cad1
fiqriachmada <achmada1998@gmail.com>
2023-05-11 11:33:28 +0700

    new updated

--- src/controller/characters/getCharacterById.js
+++ src/controller/characters/getCharacterById.js
@@ -9,9 +9,14 @@ getCharacterById.get('/:id', async (req, res) => {
 
   const timeZoneOffset = '+07:00'; // change this to your desired timezone offset
 
-  const query = `SELECT *, 
+  const query = `
+  
+    SELECT hp_character.*, hp_character_image.image_url, 
     DATE_FORMAT(CONVERT_TZ(hp_character.created_at, '+00:00', '${timeZoneOffset}'), '%Y-%m-%d %H:%i:%s') as created_at,
-    DATE_FORMAT(CONVERT_TZ(hp_character.updated_at, '+00:00', '${timeZoneOffset}'), '%Y-%m-%d %H:%i:%s') as updated_at FROM hp_character WHERE hp_character.id=?`;
+    DATE_FORMAT(CONVERT_TZ(hp_character.updated_at, '+00:00', '${timeZoneOffset}'), '%Y-%m-%d %H:%i:%s') as updated_at
+    FROM hp_character
+    LEFT JOIN hp_character_image ON hp_character.image_id = hp_character_image.id WHERE hp_character.id=?
+ `;
   const [rows] = await (await connection()).query(query, [id]);
 
   const response = {
