e666861f4547bb372dc524afe6f9f576166992bc
fiqriachmada <achmada@1998.com>
2023-05-28 18:14:09 +0700

    new

--- app.js
+++ app.js
@@ -87,6 +87,3 @@ app.listen(port, () => {
   )
 })
 
-// const listener = app.listen(process.env.PORT, function () {
-//   console.log('Your app is listening on port ' + listener.address().port);
-// });
