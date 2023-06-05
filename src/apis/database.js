c777382b5e41cc89e01e7e5e932d20db571cc87d
fiqriachmada <achmada1998@gmail.com>
2023-06-05 14:21:23 +0700

    new

--- src/apis/database.js
+++ src/apis/database.js
@@ -1,35 +1,15 @@
-// db.js
+import mysql from 'mysql2/promise';
+import dotenv from 'dotenv';
 
-// import mysql from 'mysql2/promise'
-// import dotenv from 'dotenv'
+dotenv.config();
 
-// dotenv.config()
-
-// const databaseUrl = process.env.DATABASE_URL
-
-// export default function connection () {
-//   getConnection()
-// }
-
-// async function getConnection () {
-//   console.log('Database is Connected to', databaseUrl + '\n')
-//   return await mysql.createConnection(process.env.DATABASE_URL)
-// }
-
-// db.js
-
-import mysql from 'mysql2/promise'
-import dotenv from 'dotenv'
-
-dotenv.config()
-
-const databaseUrl = process.env.DATABASE_URL
+const databaseUrl = process.env.DATABASE_URL;
 
 export const connection = async () => {
-  return await getConnection()
-}
+  return await getConnection();
+};
 
-async function getConnection () {
-  console.log('Database is Connected to', databaseUrl + '\n')
-  return await mysql.createConnection(databaseUrl)
+async function getConnection() {
+  console.log('Database is Connected to', databaseUrl + '\n');
+  return await mysql.createConnection(databaseUrl);
 }
